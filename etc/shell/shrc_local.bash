1fa8da77e6ad4fbe1686a6879b5ebe4737caf9ea
EnagaB <enaga3485@gmail.com>
2022-01-17 23:44:10 +0900

    Delete function in shrc_local.bash.

--- etc/shell/shrc_local.bash
+++ etc/shell/shrc_local.bash
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 # local config for bash/zsh
-# source in .bashrc/.zshrc
-
-# local config function
-function __shrc_local_last() {
-  :
-}
+# source this at the end of shrc.bash
 
 # EOF
