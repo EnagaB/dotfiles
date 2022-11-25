0fa68613c759947e0de0b6074cf2ab887d69c487
EnagaB <63153216+EnagaB@users.noreply.github.com>
2022-11-25 13:09:46 +0000

    Complete to add vim docker

--- etc/vim/docker/docker_build.sh
+++ etc/vim/docker/docker_build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
-cd "$SCRIPT_DIR"
+declare -r image_name="evim"
+declare -r script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 
-IMAGE=${IMAGE:-"evim"}
-DOCKERFILE=${DOCKERFILE:-"${SCRIPT_DIR}/Dockerfile"}
+IMAGE=${IMAGE:-"$image_name"}
+DOCKERFILE=${DOCKERFILE:-"${script_dir}/Dockerfile"}
 
+cd "$script_dir"
 docker build -f "$DOCKERFILE" -t "$IMAGE" .
 
 # EOF
--- etc/vim/docker/docker_run.sh
+++ etc/vim/docker/docker_run.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-IMAGE=${IMAGE:-"evim"}
+declare -r image_name="evim"
+declare -r rand8=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+# host
+declare -r script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+declare -r dotvim=~/.vim
+# docker
+declare -r docker_work_dir=/work
+declare -r docker_home=/dkrhome
 
+IMAGE=${IMAGE:-"$image_name"}
+CONTAINER_NAME=${CONTAINER_NAME:-"${image_name}_${rand8}"}
 WORK_DIR=${WORK_DIR:-$(pwd)}
-
+VIM=${VIM:-"$dotvim"}
 
 docker run -it --rm \
-    --name 
-
-
- --name ${CNAME} \
-    -u $(id -u):$(id -g) \
-    -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro \
-
-    --mount type=bind,src="$WORK_DIR",dst="$WORK_DIR" \
+    --name "$CONTAINER_NAME" \
+    -u=$(id -u):$(id -g) -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro \
+    --mount type=bind,src="$WORK_DIR",dst="$docker_work_dir" \
+    --mount type=bind,src="$VIM",dst="${docker_home}/.vim" \
+    --mount type=bind,src="${VIM}/vimrc",dst="${docker_home}/.config/nvim/init.vim" \
+    -w "$docker_work_dir" \
     "$IMAGE" \
     /bin/bash
-
 
 # EOF
