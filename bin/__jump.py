f2d6d966d111d0f0348d4ef41842f76fc05ec66c
EnagaB <enaga3485@gmail.com>
2023-02-27 00:11:24 +0900

    Change jump script

--- bin/__jump.py
+++ bin/__jump.py
@@ -2,124 +2,107 @@
 # coding: utf-8
 
 import os
+import sys
 import argparse
 from pathlib import Path
-
-def get_sortkeys(pathmap):
-    keylist = list(pathmap.keys())
-    keylist.sort()
-    return keylist
-
-# cmd functions
-def add_key(pathmap, key):
-    pathmap[key] = Path.cwd()
-
-def remove_key(pathmap, key):
-    pathmap.pop(key)
-
-def show_path(pathmap, key):
-    print(pathmap[key])
-
-def show_file(pathmap):
-    keys = get_sortkeys(pathmap)
-    maxlen = 0
-    for key in keys:
-        maxlen = max(maxlen, len(key))
-    for key in keys:
-        print("{0:{1:}}:".format(key, maxlen), pathmap[key])
-
-def output_help():
-    with open(HELP_PATH, 'r') as ihelp:
-        for line in ihelp:
-            print(line.splitlines()[0])
-
-# save load file
-def load_file(pathmap_path):
-    if not pathmap_path.is_file():
-        with pathmap_path.open(mode='w') as ifile:
-            pass
-    pathmap = {}
-    with open(pathmap_path, 'r') as ifile:
-        lnum = 0
-        for line in ifile:
-            lnum += 1
-            if (lnum % 2) == 1: # lnum is odd.
-                key = line.splitlines()[0]
-            else: # lnum is even, line is path.
-                pathmap[key] = line.splitlines()[0]
-    return pathmap
-
-def save_file(pathmap_path, pathmap):
-    keys = get_sortkeys(pathmap)
-    with open(pathmap_path, 'w') as ofile:
-        for key in keys:
-            ofile.write(key + "\n")
-            ofile.write(str(pathmap[key]) + "\n")
-
-# is exist
-def is_cmd_exist(incmd):
-    cmdlist = list(CMDS.keys())
-    if incmd in cmdlist:
-        return True
-    return False
-
-def is_cmd_exist_nokey(incmd):
-    cmdlist = list(CMDS.keys())
-    if (incmd in cmdlist) and ("nokey" in CMDS[incmd][1]):
-        return True
-    return False
-
-# arg
-def get_cmd_key(cmd_key_list):
-    key = ""
-    if len(cmd_key_list) == 0:
-        cmd = "show"
-    elif len(cmd_key_list) == 1 and is_cmd_exist_nokey(cmd_key_list[0]):
-        cmd = cmd_key_list[0]
-    elif len(cmd_key_list) == 1:
-        cmd = "jump"
-        key = cmd_key_list[0]
-    else: # len(cmd_key_list) == 2
-        cmd = cmd_key_list[0]
-        key = cmd_key_list[1]
-    if not is_cmd_exist(cmd):
-        msg = "Given command does not implemented."
-        raise ValueError(msg)
-    if is_cmd_exist_nokey(key):
-        msg = "Givn key cannot be used."
-        raise ValueError(msg)
-    return cmd, key
+from typing import Optional, Tuple, List
 
 def get_args():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Save and print path")
     parser.add_argument('pathmap_path', type=Path)
-    parser.add_argument('cmd_key_list', type=str, nargs='*')
+    parser.add_argument('cmd_key', type=str, nargs='*')
     return parser.parse_args()
 
-CMDS = {
-    "add": [add_key, ["update"]],
-    "rm": [remove_key, ["update"]],
-    "jump": [show_path, []],
-    "show": [show_file, ["nokey"]],
-    "help": [output_help, ["nopathmap", "nokey"]]
-}
-HELP_PATH = Path(os.getenv("DOTFILES")) / "src/jump/help.txt"
+def main(args):
+    jump = Jump(args.pathmap_path)
+    jump(args.cmd_key)
+    return 0
 
-def main(pathmap_path, cmd, key):
-    pathmap = load_file(pathmap_path)
-    func = CMDS[cmd][0]
-    attr = CMDS[cmd][1]
-    if ("nokey" in attr) and ("nopathmap" in attr):
-        func()
-    elif ("nokey" in attr):
-        func(pathmap)
-    else:
-        func(pathmap, key)
-    if "update" in attr:
-        save_file(pathmap_path, pathmap)
-    return
+class Jump():
+    def __init__(self,
+                 pathmap_pth: Path
+                 ) -> None:
+        self.commands = ["add", "rm", "jump", "show"]
+        self.pathmap_pth = pathmap_pth
+        self.__load_pathmap()
+        return
+
+    def __call__(self, cmd_key: List[str]) -> None:
+        cmd, key = self.__get_cmd_key(cmd_key)
+        if cmd == "add": # add or update
+            if key is None: self.__raise_key_none(cmd)
+            self.pathmap[key] = Path.cwd()
+            self.__save_pathmap()
+        elif cmd == "rm": # remove
+            if key is None: self.__raise_key_none(cmd)
+            if key not in self.pathmap:
+                msg = "The key does not exist."
+                raise RuntimeError(msg)
+            self.pathmap.pop(key)
+            self.__save_pathmap()
+        elif cmd == "jump": # print (jump)
+            if key is None: self.__raise_key_none(cmd)
+            print(str(self.pathmap[key]))
+        elif cmd == "show": # remove
+            self.__show_pathmap()
+        return
+
+    def __load_pathmap(self) -> None:
+        if not self.pathmap_pth.is_file():
+            with self.pathmap_pth.open(mode="w"):
+                pass
+        self.pathmap = {}
+        with self.pathmap_pth.open(mode="r") as ifile:
+            lnum = 0
+            for line in ifile:
+                lnum += 1
+                if (lnum % 2) == 1: # lnum is odd.
+                    key = line.rstrip()
+                else: # lnum is even, line is path.
+                    self.pathmap[key] = line.rstrip()
+        return
+
+    def __save_pathmap(self) -> None:
+        keys = sorted(list(self.pathmap.keys()))
+        with self.pathmap_pth.open(mode="w") as ofile:
+            for key in keys:
+                ofile.write(key + "\n")
+                ofile.write(str(self.pathmap[key]) + "\n")
+        return
+
+    def __get_cmd_key(self, cmd_key: List[str]) -> Tuple[str, Optional[str]]:
+        if len(cmd_key) == 0:
+            cmd = "show"
+            key = None
+        elif len(cmd_key) == 1:
+            if cmd_key[0] in self.commands:
+                cmd = cmd_key[0]
+                key = None
+            else:
+                cmd = "jump"
+                key = cmd_key[0]
+        elif len(cmd_key) == 2:
+            cmd = cmd_key[0]
+            key = cmd_key[1]
+        else:
+            msg = "Incorrect num of arguments."
+            raise RuntimeError(msg)
+        return cmd, key
+
+    def __show_pathmap(self) -> None:
+        keys = sorted(list(self.pathmap.keys()))
+        maxlen = 0
+        for key in keys:
+            maxlen = max(maxlen, len(key))
+        for key in keys:
+            print("{0:{1:}}:".format(key, maxlen), self.pathmap[key])
+        return
+
+    def __raise_key_none(self, cmd: str):
+        msg = f"When command is '{cmd}', key should be given."
+        raise RuntimeError(msg)
+        return
 
 if __name__ == '__main__':
     args = get_args()
-    cmd, key = get_cmd_key(args.cmd_key_list)
-    main(args.pathmap_path, cmd, key)
+    sys.exit(main(args))
