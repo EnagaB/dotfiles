b89bdb0329c13bfc619446cff09419af35da0c3f
EnagaB <enaga3485@gmail.com>
2024-04-04 04:57:13 +0900

    Delete local profile.ps1

--- windows/src_profile.ps1
+++ windows/src_profile.ps1
@@ -1,15 +1,7 @@
-﻿$local_profile = "${HOME}/local_profile.ps1"
-
-Import-Module PSReadLine
+﻿Import-Module PSReadLine
 Set-PSReadlineOption -EditMode Emacs
 Set-PSReadlineKeyHandler -Key Ctrl+d -Function DeleteCharOrExit
 
 Set-Alias -Name op -Value Invoke-Item
-Set-Alias -Name vi -Value nvim
-
-# local profile
-if ( -not ( Test-Path -Path "$local_profile" ) )
-{
-    New-Item -Path "$local_profile" -itemtype file
-}
-. "$local_profile"
+# Set-Alias -Name vi -Value nvim
+Set-Alias -Name vi -Value 'C:\Program Files\Neovim\bin\nvim.exe'
