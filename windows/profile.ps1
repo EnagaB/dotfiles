ba995fead87a02e3920e75f4695f094bc676e1a3
EnagaB <enaga3485@gmail.com>
2024-04-04 04:07:39 +0900

    Update profile.ps1

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -1,1 +1,15 @@
+$local_profile = "${HOME}/local_profile.ps1"
+
+Import-Module PSReadLine
+Set-PSReadlineOption -EditMode Emacs
+Set-PSReadlineKeyHandler -Key Ctrl+d -Function DeleteCharOrExit
+
 Set-Alias -Name op -Value Invoke-Item
+Set-Alias -Name vi -Value nvim
+
+# local profile
+if ( -not ( Test-Path -Path "$local_profile" ) )
+{
+    New-Item -Path "$local_profile" -itemtype file
+}
+. "$local_profile"
