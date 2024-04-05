ad83ebb965b18ca75c5c93fc56064c88dccbec94
EnagaB <enaga3485@gmail.com>
2024-04-06 08:31:57 +0900

    Update src_profile.ps1

--- windows/src_profile.ps1
+++ windows/src_profile.ps1
@@ -3,5 +3,5 @@ Set-PSReadlineOption -EditMode Emacs
 Set-PSReadlineKeyHandler -Key Ctrl+d -Function DeleteCharOrExit
 
 Set-Alias -Name op -Value Invoke-Item
-# Set-Alias -Name vi -Value nvim
 Set-Alias -Name vi -Value 'C:\Program Files\Neovim\bin\nvim.exe'
+Set-Alias -Name skr -Value "${HOME}\apps\sakura\sakura.exe"
