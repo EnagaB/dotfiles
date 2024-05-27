991973eb4b65f04b2cca156ddc5bfc9d67226cc1
EnagaB <enaga3485@gmail.com>
2024-05-28 01:52:36 +0900

    Add alias lst

--- windows/src_profile.ps1
+++ windows/src_profile.ps1
@@ -4,6 +4,10 @@ Import-Module PSReadLine
 Set-PSReadlineOption -EditMode Emacs
 Set-PSReadlineKeyHandler -Key Ctrl+d -Function DeleteCharOrExit
 
+function _ls_sort_time { Get-ChildItem | sort -Property LastWriteTime }
+
 Set-Alias -Name op -Value Invoke-Item
+Set-Alias -Name lst -Value _ls_sort_time
+
 Set-Alias -Name vi -Value 'C:\Program Files\Neovim\bin\nvim.exe'
 Set-Alias -Name skr -Value "${HOME}\apps\sakura\sakura.exe"
