a753b04c8a6f38558541ff3516e96f3eebbb409a
EnagaB <enaga3485@gmail.com>
2024-09-25 13:37:09 +0900

    Add linux-like ls to profile.ps1

--- windows/src_profile.ps1
+++ windows/src_profile.ps1
@@ -5,9 +5,20 @@ Set-PSReadlineOption -EditMode Emacs
 Set-PSReadlineKeyHandler -Key Ctrl+d -Function DeleteCharOrExit
 
 function _ls_sort_time { Get-ChildItem | sort -Property LastWriteTime }
+function _linux_ls {
+    $ls_path = 'C:\Program Files\Git\usr\bin\ls.exe'
+    if ( -not ( Test-Path -Path "$git_dir" -PathType Leaf ) )
+    {
+        Write-Error "Linux-like ls failed."
+        Write-Error "${ls_path} was not found."
+        return
+    }
+    & "$ls_path" --color=auto $args
+}
 
 Set-Alias -Name op -Value Invoke-Item
 Set-Alias -Name lst -Value _ls_sort_time
+Set-Alias -Name lls -Value _linux_ls
 
 Set-Alias -Name vi -Value 'C:\Program Files\Neovim\bin\nvim.exe'
 Set-Alias -Name skr -Value "${HOME}\apps\sakura\sakura.exe"
