cba6ba755d1bcc96b5a17b0b7495f4b2fde2f1f5
EnagaB <enaga3485@gmail.com>
2024-09-25 14:13:39 +0900

    Update windows profile.ps1

--- windows/src_profile.ps1
+++ windows/src_profile.ps1
@@ -4,28 +4,21 @@ Import-Module PSReadLine
 Set-PSReadlineOption -EditMode Emacs
 Set-PSReadlineKeyHandler -Key Ctrl+d -Function DeleteCharOrExit
 
+Set-Alias -Name op -Value Invoke-Item
 function _ls_sort_time { Get-ChildItem | sort -Property LastWriteTime }
-function _linux_ls {
-    $ls_path = 'C:\Program Files\Git\usr\bin\ls.exe'
-    if ( -not ( Test-Path -Path "$git_dir" -PathType Leaf ) )
-    {
-        Write-Error "Linux-like ls failed."
-        Write-Error "${ls_path} was not found."
-        return
-    }
-    & "$ls_path" --color=auto $args
-}
-function _su
-{
+Set-Alias -Name lst -Value _ls_sort_time
+function _su {
     Start-Process powershell.exe `
         -ArgumentList "-ExecutionPolicy RemoteSigned -NoExit -command `"& {Set-Location $HOME}`"" `
         -Verb runas
 }
-
-Set-Alias -Name op -Value Invoke-Item
-Set-Alias -Name lst -Value _ls_sort_time
-Set-Alias -Name lls -Value _linux_ls
 Set-Alias -Name su -Value _su
 
+# use third-party apps
+function _linux_ls {
+    $ls_path = 'C:\Program Files\Git\usr\bin\ls.exe'
+    & "$ls_path" --color=auto $args
+}
+Set-Alias -Name lls -Value _linux_ls
 Set-Alias -Name vi -Value 'C:\Program Files\Neovim\bin\nvim.exe'
 Set-Alias -Name skr -Value "${HOME}\apps\sakura\sakura.exe"
